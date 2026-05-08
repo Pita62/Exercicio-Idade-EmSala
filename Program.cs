e37091513ecb2b30aa38ce2b0de38bcf9c9da065
Pita62 <guaru.guerreiro@gmil.com>
2026-05-08 20:33:30 -0300

    Exercício realizado e funcionando.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite sua idade:");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+if (idade >= 18)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Você é maior de idade");
+} 
+else
+{
+  Console.ForegroundColor = ConsoleColor.Red;
+  Console.WriteLine("Você é menor de idade.");
+}
+Console.ResetColor();
+Console.WriteLine("Obrigado!");
+
+    
+
+
+
